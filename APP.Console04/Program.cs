21f4d007c789b4dc281054b2ba4b5b04c70db03a
Jeferson da Silva Araujo <jeff.araujo@gmail.com>
2018-09-24 22:51:04 -0300

    Add project files.

---
+++ APP.Console04/Program.cs
@@ -0,0 +1,150 @@
+﻿using HtmlAgilityPack;
+using Polly;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+using System.Web;
+
+namespace APP.Projeto04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cenario_1();
+            //Cenario_2();
+            //Cenario_3();
+            //Cenario_4();
+            //Cenario_5();
+            Cenario_6();
+            Console.ReadLine();
+        }
+
+        private static void Cenario_6()
+        {
+            Policy.Handle<WebException>()
+                .WaitAndRetry(
+                new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(10),
+                    TimeSpan.FromSeconds(25),
+                },
+                (error, tempo) =>
+                {
+                    Debug.WriteLine($"Tipo de exceção: {error.GetType().Name}, Executado depois de: {tempo}. Horário: {DateTime.Now.ToString() }");
+                })
+                .Execute(() =>
+                {
+
+                    using (var webclient = new WebClient())
+                    {
+                        string conexao = webclient.DownloadString("https://fiapmobappsfsm.azurewebsites.net/");
+                        HtmlDocument htmlParse = new HtmlDocument();
+                        htmlParse.LoadHtml(conexao);
+
+                        Console.WriteLine(htmlParse.DocumentNode.SelectSingleNode("//title").InnerText);
+                    }
+                });
+
+        }
+
+        private static void Cenario_5()
+        {
+            Policy.Handle<WebException>()
+                .Retry(10, (error, retryCount) =>
+                {
+                    Debug.WriteLine(error.Message);
+                    Debug.WriteLine($"Ocorreu um erro inesperado. Tentativas: {retryCount}");
+                })
+                .Execute(() =>
+                {
+                    Thread.Sleep(1000);
+                    using (var webclient = new WebClient())
+                    {
+                        string conexao = webclient.DownloadString("https://fiapmobappsfsm.azurewebsites.net/");
+                        HtmlDocument htmlParse = new HtmlDocument();
+                        htmlParse.LoadHtml(conexao);
+
+                        Console.WriteLine(htmlParse.DocumentNode.SelectSingleNode("//title").InnerText);
+                    }
+                });
+        }
+
+        private static void Cenario_4()
+        {
+            Policy.Handle<WebException>()
+                .RetryForever(error => 
+                {
+                    Debug.WriteLine(error.Message);
+                })
+                .Execute(() =>
+                {
+                    using (var webclient = new WebClient())
+                    {
+                        string conexao = webclient.DownloadString("https://fiapmobappsfsm.azurewebsites.net/");
+                        HtmlDocument htmlParse = new HtmlDocument();
+                        htmlParse.LoadHtml(conexao);
+
+                        Console.WriteLine(htmlParse.DocumentNode.SelectSingleNode("//title").InnerText);
+                    }
+                });
+        }
+
+        private static void Cenario_3()
+        {
+            bool isExecutado = false;
+
+            while (!isExecutado)
+            {
+                try
+                {
+                    using (var webclient = new WebClient())
+                    {
+                        string conexao = webclient.DownloadString("https://fiapmobappsfsm.azurewebsites.net/");
+                        HtmlDocument htmlParse = new HtmlDocument();
+                        htmlParse.LoadHtml(conexao);
+
+                        Console.WriteLine(htmlParse.DocumentNode.SelectSingleNode("//title").InnerText);
+                        isExecutado = true;
+                    }
+                }
+                catch (WebException)
+                {
+                    Debug.WriteLine("Ocorreu um erro inesperado.");
+                }
+            }
+        }
+
+        private static void Cenario_2()
+        {
+            try
+            {
+                var webclient = new WebClient();
+
+                string conexao = webclient.DownloadString("https://fiapmobappsfsm.azurewebsites.net/");
+                HtmlDocument htmlParse = new HtmlDocument();
+                htmlParse.LoadHtml(conexao);
+
+                Console.WriteLine(htmlParse.DocumentNode.SelectSingleNode("//title").InnerText);
+            }
+            catch (WebException)
+            {
+                Console.WriteLine("Ocorreu um erro inesperado.");
+            }
+        }
+
+        private static void Cenario_1()
+        {
+            var webclient = new WebClient();
+
+            string conexao = webclient.DownloadString("https://fiapmobappsfsm.azurewebsites.net/");
+            HtmlDocument htmlParse = new HtmlDocument();
+            htmlParse.LoadHtml(conexao);
+
+            Console.WriteLine(htmlParse.DocumentNode.SelectSingleNode("//title").InnerText);
+        }
+    }
+}
